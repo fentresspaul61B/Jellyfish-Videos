650b7a2d803e249e5c6c8985b7c075527979fa7c
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-11-11 17:08:36 -0800

    added start to API.

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, UploadFile
+from upload_video import upload_video
+import os
+import shutil
+import tempfile
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        shutil.copyfileobj(file.file, temp_file)
+        temp_file_path = temp_file.name  # Capture the temp file path before the file is closed
+
+    # Now that the file is saved, you can pass the path to upload_video
+    response = upload_video(
+        file_path=temp_file_path, 
+        title="Uploaded from FastAPI",
+        description="A video uploaded from FastAPI",
+        category="22",
+        keywords="test, FastAPI",
+        privacyStatus="private"
+    )
+
+    # Clean up the temp file after upload
+    os.unlink(temp_file_path)
+
+    # Return some response or the YouTube video link
+    return response
+
---
+++ upload_video.py
@@ -0,0 +1,220 @@
+#!/usr/bin/python
+
+import httplib2
+import os
+import random
+import sys
+import time
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+
+
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the Google API Console at
+# https://console.cloud.google.com/.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This OAuth 2.0 access scope allows an application to upload files to the
+# authenticated user's YouTube channel, but doesn't allow other types of access.
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+SCOPES = [YOUTUBE_UPLOAD_SCOPE]
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the API Console
+https://console.cloud.google.com/
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
+
+def get_authenticated_service():
+    # Load credentials from a file
+    credentials = None
+    token_json = 'token.json'  # Path to the token.json file
+
+    if os.path.exists(token_json):
+        credentials = Credentials.from_authorized_user_file(token_json, SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES)
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_json, 'w') as token:
+            token.write(credentials.to_json())
+
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
+
+
+
+def resumable_upload(insert_request):
+    response = None
+    error = None
+    retry = 0
+    while response is None:
+        try:
+            print("Uploading file...")
+            status, response = insert_request.next_chunk()
+            if response is not None:
+                if 'id' in response:
+                    print("Video id '%s' was successfully uploaded." %
+                          response['id'])
+                else:
+                    exit("The upload failed with an unexpected response: %s" % response)
+        except HttpError as e:
+            if e.resp.status in RETRIABLE_STATUS_CODES:
+                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                                     e.content)
+            else:
+                raise
+        except RETRIABLE_EXCEPTIONS as e:
+            error = "A retriable error occurred: %s" % e
+
+        if error is not None:
+            print(error)
+            retry += 1
+            if retry > MAX_RETRIES:
+                exit("No longer attempting to retry.")
+
+            max_sleep = 2 ** retry
+            sleep_seconds = random.random() * max_sleep
+            print("Sleeping %f seconds and then retrying..." % sleep_seconds)
+            time.sleep(sleep_seconds)
+
+
+
+
+from googleapiclient.errors import HttpError
+
+def upload_video(
+        file_path:     str,
+        title:         str = "test_video #Shorts",
+        description:   str = "test_video #Shorts",
+        category:      str = "28",
+        keywords:      str = "Shorts",
+        privacyStatus: str = "private"
+    ):
+
+    if not os.path.exists(file_path):
+        print("Please specify a valid file.")
+        return None
+
+    youtube = get_authenticated_service()
+    
+    tags = keywords.split(",") if keywords else None
+    body = {
+        'snippet': {
+            'title': title,
+            'description': description,
+            'tags': tags,
+            'categoryId': category
+        },
+        'status': {
+            'privacyStatus': privacyStatus 
+        }
+    }
+
+    try:
+        video_id = initialize_upload(youtube, file_path, body)
+        youtube_link = f"https://www.youtube.com/watch?v={video_id}"
+        print(f"Video uploaded. Link: {youtube_link}")
+        return youtube_link
+    except HttpError as e:
+        print(f"An HTTP error {e.resp.status} occurred:\n{e.content}")
+        return None
+
+# You would also need to modify get_authenticated_service to not take any arguments.
+# And here's how you would modify initialize_upload:
+
+def initialize_upload(youtube, file_path, body):
+    # Call the API's videos.insert method to create and upload the video.
+    insert_request = youtube.videos().insert(
+        part=",".join(body.keys()),
+        body=body,
+        media_body=MediaFileUpload(file_path, chunksize=-1, resumable=True)
+    )
+
+    # Execute the upload and return the video ID
+    return resumable_upload(insert_request)
+
+
+# This method implements an exponential backoff strategy to resume a
+# failed upload.
+
+def initialize_upload(youtube, file_path, body):
+    insert_request = youtube.videos().insert(
+        part="snippet,status",
+        body=body,
+        media_body=MediaFileUpload(file_path, resumable=True)
+    )
+
+    # Upload the file and get the response
+    response = None
+    while response is None:
+        status, response = insert_request.next_chunk()
+
+        if 'id' in response:
+            return response['id']
+        else:
+            # If there's no ID in the response, handle the error appropriately
+            print("The upload failed with an unexpected response: %s" % response)
+            return None
+
+
+
+# The resumable_upload function would remain mostly the same, 
+# but it would return the video ID on success instead of printing it.
+
+
+if __name__ == '__main__':
+   upload_video("/Users/paulfentress/Downloads/youtube_short_asdf.mp4") 
